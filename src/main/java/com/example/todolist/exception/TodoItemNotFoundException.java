94a6f7a468488d7b7d1510e7bf750afa8340a1de
omagara <omagara@oocl.com>
2021-08-19 21:52:12 +0800

    [TodoList-Backend][Ramon]feat: added TodoItemNotFoundException class

---
+++ src/main/java/com/example/todolist/exception/TodoItemNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.todolist.exception;
+
+public class TodoItemNotFoundException extends RuntimeException{
+
+    public TodoItemNotFoundException(String message) {
+        super(message);
+    }
+}
