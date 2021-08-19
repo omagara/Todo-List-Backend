2b2a180fdd90871a17eef8bf3142c0a96d8cdde0
omagara <omagara@oocl.com>
2021-08-19 13:55:43 +0800

    [TodoList-Backend][Ramon]feat: impelemented getAllTodoItems in the TodoListService

--- src/main/java/service/TodoListService.java
+++ src/main/java/service/TodoListService.java
@@ -1,2 +1,22 @@
-package service;public class TodoListService {
+package service;
+
+import entity.TodoItem;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
+import repository.TodoListRepository;
+
+import java.util.List;
+
+@Service
+public class TodoListService {
+
+    private TodoListRepository todoListRepository;
+
+    public TodoListService(TodoListRepository todoListRepository) {
+        this.todoListRepository = todoListRepository;
+    }
+
+    public List<TodoItem> getAllTodoItems(){
+        return todoListRepository.findAll();
+    }
 }
