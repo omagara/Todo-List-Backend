68bfbeb98716a339b9026fd755c327e864f1e75b
omagara <omagara@oocl.com>
2021-08-19 14:02:18 +0800

    [TodoList-Backend][Ramon]feat: added autowired annotation in the TodoListRepository

--- src/main/java/service/TodoListService.java
+++ src/main/java/service/TodoListService.java
@@ -1,6 +1,7 @@
 package service;
 
 import entity.TodoItem;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
 import repository.TodoListRepository;
@@ -9,7 +10,7 @@ import java.util.List;
 
 @Service
 public class TodoListService {
-
+    @Autowired
     private TodoListRepository todoListRepository;
 
     public TodoListService(TodoListRepository todoListRepository) {
