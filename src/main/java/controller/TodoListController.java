9f2240775ee6218365f6033c0a9589a5dccbc2ed
omagara <omagara@oocl.com>
2021-08-19 14:03:21 +0800

    [TodoList-Backend][Ramon]feat: created TodoListController class; added getAllTodoItems API

--- src/main/java/controller/TodoListController.java
+++ src/main/java/controller/TodoListController.java
@@ -1,2 +1,26 @@
-package controller;public class TodoListController {
+package controller;
+
+import entity.TodoItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import service.TodoListService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoListController {
+    @Autowired
+    private TodoListService todoListService;
+
+    public TodoListController(TodoListService todoListService) {
+        this.todoListService = todoListService;
+    }
+
+    @GetMapping
+    public List<TodoItem> getAllTodoItems(){
+        return todoListService.getAllTodoItems();
+    }
 }
