79de7b832028fd1b932506c10744b1da7d6feeaf
omagara <omagara@oocl.com>
2021-08-19 21:14:51 +0800

    [TodoList-Backend][Ramon]refactor: removed the duplicate java classes

--- src/main/java/repository/TodoListRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import entity.TodoItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TodoListRepository extends JpaRepository <TodoItem, Integer> {
-}
