3f1baff09db7487f1fadabd1d198f291e9a3f9f2
omagara <omagara@oocl.com>
2021-08-21 15:16:03 +0800

    [TodoList-Backend][Ramon]feat: created toResponse function in TodoMapper

--- src/main/java/com/example/todolist/mapper/TodoMapper.java
+++ src/main/java/com/example/todolist/mapper/TodoMapper.java
@@ -1,6 +1,7 @@
 package com.example.todolist.mapper;
 
 import com.example.todolist.dto.TodoRequest;
+import com.example.todolist.dto.TodoResponse;
 import com.example.todolist.entity.Todo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
@@ -11,5 +12,11 @@ public class TodoMapper {
         Todo todo = new Todo();
         BeanUtils.copyProperties(todoRequest, todo);
         return todo;
+    }
+
+    public TodoResponse toResponse(Todo todo){
+        TodoResponse todoResponse = new TodoResponse();
+        BeanUtils.copyProperties(todo, todoResponse);
+        return todoResponse;
     }
 }
