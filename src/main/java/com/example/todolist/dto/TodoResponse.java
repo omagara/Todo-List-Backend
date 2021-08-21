db961c900c9256bdf3b95c68470d40e4257b237e
omagara <omagara@oocl.com>
2021-08-21 15:09:13 +0800

    [TodoList-Backend][Ramon]feat: created TodoResponse for data transfer object

---
+++ src/main/java/com/example/todolist/dto/TodoResponse.java
@@ -0,0 +1,46 @@
+package com.example.todolist.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class TodoResponse {
+
+    private int id;
+    private String text;
+    private boolean done;
+
+    public TodoResponse(String text, boolean done) {
+        this.text = text;
+        this.done = done;
+    }
+
+    public TodoResponse(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
